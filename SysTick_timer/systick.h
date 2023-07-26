8d60e9ab9b6f7cca9b3e2b13aa79804dd43c28c6
Raishul-Islam-Embedded <raishul2019@gmail.com>
2023-07-26 17:34:04 +0600

    Systick Timer driver added

---
+++ SysTick_timer/systick.h
@@ -0,0 +1,14 @@
+/*
+ * systick.h
+ *
+ *  Created on: Jul 26, 2023
+ *      Author: Raishul Islam
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void systickDelayMs(int delay);
+
+
+#endif /* SYSTICK_H_ */
